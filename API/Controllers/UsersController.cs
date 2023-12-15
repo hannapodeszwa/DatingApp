27e5a1e8b61c79b9f045bc5773722bf6bbe94682
hannapodeszwa <hanna.podeszwa@gmail.com>
2023-12-15 09:58:20 +0100

    section 3 task 25

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,11 +1,13 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
     [ApiController]
+   // [EnableCors("OpenCORSPolicy")]
     [Route("api/[controller]")] // /api/users
     public class UsersController : ControllerBase
     {
--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 
 using API.Data;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,12 +14,22 @@ builder.Services.AddDbContext<DataContext> (opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));      
 });
 
+//builder.Services.AddCors();
+builder.Services.AddCors(options => {
+    options.AddDefaultPolicy(policy => policy.AllowAnyHeader()
+        .AllowAnyMethod().WithOrigins("https://localhost:4200"));
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseHttpsRedirection();
+//app.UseCors(x => x.AllowAnyHeader() .AllowAnyMethod() /*.AllowCredentials() */.WithOrigins("http://localhost:4200"));  
+//app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+app.UseCors();
 
-app.UseAuthorization();
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
 
 app.MapControllers();
 
